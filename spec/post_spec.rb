e162916ec88ee7e0bad945e71dc76d70eef9ba04
Ben Lovell <benjamin.lovell@gmail.com>
2015-05-13 16:30:37 +0100

    Adds empty specs for `Post`

---
+++ spec/post_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'helper'
+
+describe Post do
+  it 'belongs to an author'
+
+  it 'has many comments'
+
+  it 'belongs to a category'
+
+  it 'belongs to a blog through a category'
+
+  it 'has many tags through posttags'
+end
